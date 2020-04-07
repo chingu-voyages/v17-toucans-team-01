250b92d7de3ecab84bb238e2a3e0dc12800393a8
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-04-06 19:16:12 -0700

    latest build

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import setCountdown from './countdown';
-import accordion from './accordion'
-import glide from './slider'
-import './scss/styles.scss'
+import accordion from './accordion';
+import glide from './slider';
+import '../scss/styles.scss';
 
 // Update Dom
 accordion();
